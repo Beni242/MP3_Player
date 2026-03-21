dcd6a0f971bda88f1a9a6f47e3c93e4f8c9ee792
MBD <divinemountso@gmail.com>
2026-03-21 23:36:15 +0300

    first finition

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter
 import customtkinter
 import pygame
 from PIL import Image, ImageTk
+from threading import Thread
 import time
 import math
 
@@ -16,23 +17,80 @@ root.geometry("400x500")  # Fixed: use x instead of *
 
 pygame.mixer.init()
 
-list_of_songs = ['music/City.wav']
-list_of_covers = ['img/city.jpg'] 
+list_of_songs = ['music/Alex Warren.mp3', "music/One_Direction.mp3"]
+list_of_covers = ['img/photo_1.png', 'img/photo_2.png'] 
+current_song_index = 0
+
+
+def get_album_cover(song_index):
+    image1 = Image.open(list_of_covers[current_song_index])
+    image2 = image1.resize((250, 250))
+    load = ImageTk.PhotoImage(image2)
+    label1 = tkinter.Label(root, image=load)
+    label1.image = load
+    label1.place(relx=.19, rely=.06)
+   # Extract song name (remove 'music/' prefix)
+    song_name = list_of_songs[song_index].replace('music/', '').replace('.mp3', '')
+    song_name_label = tkinter.Label(root, text=song_name, bg="#222222", fg='white')
+    song_name_label.place(relx=0.4, rely=0.6)
+
+
+ #Update progress bar
+def progress():
+    try:
+       # Get the current song's length in seconds
+        song = pygame.mixer.Sound(list_of_songs[current_song_index])
+        song_length = song.get_length()  # Total length in seconds
+        
+        while pygame.mixer.music.get_busy():
+            if pygame.mixer.music.get_pos() > 0:
+                current_pos = pygame.mixer.music.get_pos() / 1000  # Convert ms to seconds
+                # Calculate percentage (0.0 to 1.0)
+                if song_length > 0:
+                    progress_percentage = current_pos / song_length
+                    progressbar.set(progress_percentage)
+            time.sleep(0.5)
+    except:
+        pass
+
+
+def threading():
+    t1 = Thread(target=progress, daemon=True)
+    t1.start()
+
 
 #funtion to play music
 def play_music():
-    pass
+   
+    global current_song_index
+
+    threading()
+    
+    song_name = list_of_songs[current_song_index]
+    pygame.mixer.music.load(song_name)
+    pygame.mixer.music.play(loops=0)
+    pygame.mixer.music.set_volume(.5)
+    get_album_cover(current_song_index)
+
+    
+    
 
 
 def skip_forward():
-    pass
+     global current_song_index
+     current_song_index = (current_song_index + 1) % len(list_of_songs)
+     play_music()
 
 
 def skip_backward():
-    pass
+     global current_song_index
+     current_song_index = (current_song_index - 1) % len(list_of_songs)
+     play_music()
 
-def volume():
-    pass
+def volume(value):
+    pygame.mixer.music.set_volume(float(value))
+
+
 #buttons
 
 # Play button (center)
@@ -52,6 +110,10 @@ skip_b.place(relx=0.2, rely=0.7, anchor=tkinter.CENTER)
 slider = customtkinter.CTkSlider(master=root, from_=0, to=1, command=volume, width=200)
 slider.place(relx=0.5, rely=0.78, anchor=tkinter.CENTER)
 
+#progress bar
+
+progressbar = customtkinter.CTkProgressBar(master=root, progress_color="#ededf6", width=200)
+progressbar.place(relx=.5, rely=.85, anchor=tkinter.CENTER)
 
 
 
